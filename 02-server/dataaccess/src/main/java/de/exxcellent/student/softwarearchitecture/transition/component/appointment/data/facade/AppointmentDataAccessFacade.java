804b61aa9592f5ecee7194fe4ec0f4f560045928
Andre Lehnert <andre.lehnert@exxcellent.de>
2020-06-09 12:43:03 +0200

    Common crud facade and mapping

--- 02-server/dataaccess/src/main/java/de/exxcellent/student/softwarearchitecture/transition/component/appointment/data/facade/AppointmentDataAccessFacade.java
+++ 02-server/dataaccess/src/main/java/de/exxcellent/student/softwarearchitecture/transition/component/appointment/data/facade/AppointmentDataAccessFacade.java
@@ -1,9 +1,10 @@
 package de.exxcellent.student.softwarearchitecture.transition.component.appointment.data.facade;
 
+import de.exxcellent.student.softwarearchitecture.transition.common.dataaccess.CrudDataAccessFacade;
 import de.exxcellent.student.softwarearchitecture.transition.common.dataaccess.User;
-import de.exxcellent.student.softwarearchitecture.transition.common.errorhandling.ErrorCode;
-import de.exxcellent.student.softwarearchitecture.transition.common.errorhandling.exception.BusinessException;
+import de.exxcellent.student.softwarearchitecture.transition.common.datetime.DateTimeUtil;
 import de.exxcellent.student.softwarearchitecture.transition.component.appointment.data.AppointmentRepository;
+import de.exxcellent.student.softwarearchitecture.transition.component.appointment.data.entities.AppointmentEntity;
 import de.exxcellent.student.softwarearchitecture.transition.component.appointment.data.mapper.AppointmentDataAccessMapper;
 import de.exxcellent.student.softwarearchitecture.transition.component.appointment.dataaccess.AppointmentDataAccess;
 import de.exxcellent.student.softwarearchitecture.transition.component.appointment.dataaccess.types.AppointmentDTO;
@@ -11,51 +12,44 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Component
-public class AppointmentDataAccessFacade implements AppointmentDataAccess {
-
-    private final AppointmentRepository appointmentRepository;
+public class AppointmentDataAccessFacade extends CrudDataAccessFacade<AppointmentEntity> implements AppointmentDataAccess {
 
     @Autowired
-    public AppointmentDataAccessFacade(AppointmentRepository appointmentRepository) {
-        this.appointmentRepository = appointmentRepository;
+    public AppointmentDataAccessFacade(AppointmentRepository appointmentRepository, DateTimeUtil dateTimeUtil) {
+       super(appointmentRepository, dateTimeUtil);
     }
 
     @Override
     public List<AppointmentDTO> findAll() {
-        var entities = appointmentRepository.findAll();
+        var entities = findAllEntities();
         return entities.stream().map(AppointmentDataAccessMapper.toAppointmentDTO).collect(Collectors.toList());
     }
 
     @Override
     public AppointmentDTO findById(Long id) {
-        var entity = appointmentRepository.findById(id).orElseThrow(() -> new BusinessException(ErrorCode.ENTITY_NOT_FOUND_ERROR,
-                String.format("Entity with id '%s' not found.", id), id));
+        var entity = findEntityById(id);
         return AppointmentDataAccessMapper.toAppointmentDTO.apply(entity);
     }
 
     @Override
     public AppointmentDTO create(AppointmentDTO newDTO, User user) {
         var entity = AppointmentDataAccessMapper.toAppointmentEntity.apply(newDTO);
-
-        return null;
+        var createdEntity = createEntity(entity, user);
+        return AppointmentDataAccessMapper.toAppointmentDTO.apply(createdEntity);
     }
 
     @Override
     public AppointmentDTO update(AppointmentDTO updatedDTO, User user) {
-        return null;
+        var entity = AppointmentDataAccessMapper.toAppointmentEntity.apply(updatedDTO);
+        var updatedEntity = createEntity(entity, user);
+        return AppointmentDataAccessMapper.toAppointmentDTO.apply(updatedEntity);
     }
 
     @Override
-    public void deleteById(Long processId) {
-
-    }
-
-    @Override
-    public AppointmentDTO saveAndFlush(AppointmentDTO entity) {
-        return null;
+    public void deleteById(Long id) {
+       deleteEntity(id);
     }
 }
