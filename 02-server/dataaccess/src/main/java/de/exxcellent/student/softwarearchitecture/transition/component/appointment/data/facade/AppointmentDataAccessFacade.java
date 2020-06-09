9cee3188a1db31275d277c47f3371266030a9eed
Andre Lehnert <andre.lehnert@exxcellent.de>
2020-06-09 12:48:38 +0200

    Use map struct mapper

--- 02-server/dataaccess/src/main/java/de/exxcellent/student/softwarearchitecture/transition/component/appointment/data/facade/AppointmentDataAccessFacade.java
+++ 02-server/dataaccess/src/main/java/de/exxcellent/student/softwarearchitecture/transition/component/appointment/data/facade/AppointmentDataAccessFacade.java
@@ -5,7 +5,7 @@ import de.exxcellent.student.softwarearchitecture.transition.common.dataaccess.U
 import de.exxcellent.student.softwarearchitecture.transition.common.datetime.DateTimeUtil;
 import de.exxcellent.student.softwarearchitecture.transition.component.appointment.data.AppointmentRepository;
 import de.exxcellent.student.softwarearchitecture.transition.component.appointment.data.entities.AppointmentEntity;
-import de.exxcellent.student.softwarearchitecture.transition.component.appointment.data.mapper.AppointmentDataAccessMapper;
+import de.exxcellent.student.softwarearchitecture.transition.component.appointment.data.mapper.AppointmentDataAccessMapperImpl;
 import de.exxcellent.student.softwarearchitecture.transition.component.appointment.dataaccess.AppointmentDataAccess;
 import de.exxcellent.student.softwarearchitecture.transition.component.appointment.dataaccess.types.AppointmentDTO;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,35 +17,40 @@ import java.util.stream.Collectors;
 @Component
 public class AppointmentDataAccessFacade extends CrudDataAccessFacade<AppointmentEntity> implements AppointmentDataAccess {
 
+    private final AppointmentDataAccessMapperImpl appointmentDataAccessMapper;
+
     @Autowired
-    public AppointmentDataAccessFacade(AppointmentRepository appointmentRepository, DateTimeUtil dateTimeUtil) {
-       super(appointmentRepository, dateTimeUtil);
+    public AppointmentDataAccessFacade(AppointmentRepository appointmentRepository,
+                                       DateTimeUtil dateTimeUtil,
+                                       AppointmentDataAccessMapperImpl appointmentDataAccessMapper) {
+        super(appointmentRepository, dateTimeUtil);
+        this.appointmentDataAccessMapper = appointmentDataAccessMapper;
     }
 
     @Override
     public List<AppointmentDTO> findAll() {
         var entities = findAllEntities();
-        return entities.stream().map(AppointmentDataAccessMapper.toAppointmentDTO).collect(Collectors.toList());
+        return entities.stream().map(appointmentDataAccessMapper::toDTO).collect(Collectors.toList());
     }
 
     @Override
     public AppointmentDTO findById(Long id) {
         var entity = findEntityById(id);
-        return AppointmentDataAccessMapper.toAppointmentDTO.apply(entity);
+        return appointmentDataAccessMapper.toDTO(entity);
     }
 
     @Override
     public AppointmentDTO create(AppointmentDTO newDTO, User user) {
-        var entity = AppointmentDataAccessMapper.toAppointmentEntity.apply(newDTO);
+        var entity = appointmentDataAccessMapper.toEntity(newDTO);
         var createdEntity = createEntity(entity, user);
-        return AppointmentDataAccessMapper.toAppointmentDTO.apply(createdEntity);
+        return appointmentDataAccessMapper.toDTO(entity);
     }
 
     @Override
     public AppointmentDTO update(AppointmentDTO updatedDTO, User user) {
-        var entity = AppointmentDataAccessMapper.toAppointmentEntity.apply(updatedDTO);
+        var entity = appointmentDataAccessMapper.toEntity(updatedDTO);
         var updatedEntity = createEntity(entity, user);
-        return AppointmentDataAccessMapper.toAppointmentDTO.apply(updatedEntity);
+        return appointmentDataAccessMapper.toDTO(entity);
     }
 
     @Override
